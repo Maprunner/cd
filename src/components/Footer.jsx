885e4d397515cfe0965ed683c6f5c49eaf22bc5b
Maprunner <simon@maprunner.co.uk>
2020-04-07 19:42:19 +0100

    Add German translation
    Closes #28

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
     <Navbar bg="dark" variant="dark" className="mt-2">
       <div className="container">
         <Navbar.Text>
-          Version 2.0.3
+          Version 2.0.4
         </Navbar.Text>
       </div>
     </Navbar>
