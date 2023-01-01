b30991df3ab21976e399263a7f8dcd4f8986168d
Simon Errington <simon@maprunner.co.uk>
2023-01-01 15:50:27 +0000

    Dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import './css/cd.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import Quiz from './components/Quiz.jsx';
+import "./css/cd.css"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
+import reportWebVitals from "./reportWebVitals"
+import Quiz from "./components/Quiz.jsx"
 
-ReactDOM.render(
-  <Quiz />,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  <React.StrictMode>
+    <Quiz />
+  </React.StrictMode>
+)
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
