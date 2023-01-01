c80da16a0c7d15fd8387e6ac0f10e10bfbbb88b9
Simon Errington <simon@maprunner.co.uk>
2023-01-01 15:50:48 +0000

    Dependencies (#55)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
+import React from "react"
+import Navbar from "react-bootstrap/Navbar"
 
-function Footer ()  {
+function Footer() {
   return (
     <Navbar bg="dark" variant="dark" className="mt-2">
       <div className="container">
-        <Navbar.Text>
-          Version 2.2.0
-        </Navbar.Text>
+        <Navbar.Text>Version 2.3.0</Navbar.Text>
       </div>
     </Navbar>
   )
